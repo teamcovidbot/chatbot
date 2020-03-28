b740cc541536beb7498a568ef6494983e7f376c4
Nxtra <nickvanhoof1@gmail.com>
2020-03-28 22:27:41 +0100

    calculate score, add image depending on result

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -1,5 +1,5 @@
 import json
-import os
+import  os
 
 def covidbot_result(request):
     """Responds to any HTTP request.
@@ -10,11 +10,6 @@ def covidbot_result(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    request_json = request.get_json(silent=True)
-    # Next lines is to enable live testing in Google Cloud Functions via Testing tab, 'Triggering event'
-    payload = request_json
-    if payload and payload.get('Memory'):
-        payload = payload.get('Memory')
     # Next line is how we expect real data to come in from Twilio
     if request.form and request.form.get('Memory'):
         payload = request.form.get('Memory')
@@ -25,163 +20,188 @@ def covidbot_result(request):
     given_answers = parse_twilio(payload)
     correct_answers = get_correct_answers(get_questions())
     score = calculate_score(given_answers, correct_answers)
+    image_url = determine_image(score)
+
+    return json.dumps({
+        "actions": [
+            {
+                "say": "response"
+            },
+            {
+                "show": {
+                    "body": "Mascot",
+                    "images": [
+                        {
+                            "label": "Mascot",
+                            "url": f"{image_url}"
+                        }
+                    ]
+                }
+            }
+        ]
+    })
 
 
-    return json.dumps({"actions": [{"say" : f"your score is {score}"}]})
+def determine_image(score):
+    if(score < 6):
+        return os.environ["BAD_IMAGE_URL"]
+    else:
+        return os.environ["GOOD_IMAGE_URL"]
+
 
 def get_questions():
-    # TODO: Read in questions from a file
     return QUESTIONS
+
 
 QUESTIONS = {
     "questions": [{
-			"name": "question1",
-            "question": "I am allowed to go outside for which case?",
-            "answers": [{
-                    "order": "D",
-                    "answer": "I need to pick up my medicine from the pharmacist.",
-                    "correct": True
-                }, {
-                    "order": "A",
-                    "answer": "I'm going for a jog with my friends in the parc.",
-                    "correct": False
-                }, {
-                    "order": "C",
-                    "answer": "We have an appointment with the bank regarding our investment plan.",
-                    "correct": False
-                }, {
-                    "order": "B",
-                    "answer": "We are taking the kids to the beach for the day.",
-                    "correct": False
-                }
-            ]
+        "name": "question1",
+        "question": "I am allowed to go outside for which case?",
+        "answers": [{
+            "order": "D",
+            "answer": "I need to pick up my medicine from the pharmacist.",
+            "correct": True
         }, {
-			"name": "question2",
-            "question": "Which bike trip am I allowed to do?",
-            "answers": [{
-                    "order": "A",
-                    "answer": "60 kilometers all by myself.",
-                    "correct": True
-                }, {
-                    "order": "C",
-                    "answer": "40 kilometers with a group of cyclists.",
-                    "correct": False
-                }, {
-                    "order": "B",
-                    "answer": "10 kilometers with the whole family on Sunday.",
-                    "correct": False
-                }, {
-                    "order": "D",
-                    "answer": "A bike trip to the parc to enjoy a sunny picnic.",
-                    "correct": False
-                }
-            ]
+            "order": "A",
+            "answer": "I'm going for a jog with my friends in the parc.",
+            "correct": False
         }, {
-			"name": "question3",
-            "question": "When do I have the obligation to wash my hands?",
-            "answers": [{
-                    "order": "A",
-                    "answer": "When I come home from the store.",
-                    "correct": True
-                }, {
-                    "order": "C",
-                    "answer": "Every half hour.",
-                    "correct": False
-                }, {
-                    "order": "D",
-                    "answer": "In the morning when I wake up.",
-                    "correct": False
-                }, {
-                    "order": "B",
-                    "answer": "I just have to make sure to wash them at least 10 times a day",
-                    "correct": False
-                }
-            ]
+            "order": "C",
+            "answer": "We have an appointment with the bank regarding our investment plan.",
+            "correct": False
         }, {
-			"name": "question4",
-            "question": "Which remedies are helpful to protect yourself against COVID-19?",
-            "answers": [{
-                    "order": "B",
-                    "answer": "Stricltly follow government regulations.",
-                    "correct": True
-                }, {
-                    "order": "A",
-                    "answer": "Absorb an unusually large amount of vitamin C.",
-                    "correct": False
-                }, {
-                    "order": "C",
-                    "answer": "Drink lots of alcohol.",
-                    "correct": False
-                }, {
-                    "order": "D",
-                    "answer": "Smoke sigarettes.",
-                    "correct": False
-                }
-            ]
-        }, {
-			"name": "question5",
-            "question": "Children are still allowed to ..",
-            "answers": [{
-                    "order": "B",
-                    "answer": "Play in the garden with other children of the same household.",
-                    "correct": True
-                }, {
-                    "order": "A",
-                    "answer": "Play in the garden with the neighbours' children.",
-                    "correct": False
-                }, {
-                    "order": "D",
-                    "answer": "Play with other children on the terrains of the local youth movement.",
-                    "correct": False
-                }, {
-                    "order": "C",
-                    "answer": "Play in a public outdoor playground with children of the same household.",
-                    "correct": False
-                }
-            ]
-        }, {
-			"name": "question6",
-            "question": "What do you do when you see that others are not following the rules?",
-            "answers": [{
-                    "order": "D",
-                    "answer": "I instruct them to behave differently and contact local authorities when they don't want to listen.",
-                    "correct": True
-                }, {
-                    "order": "B",
-                    "answer": "I cheerfully join them.",
-                    "correct": False
-                }, {
-                    "order": "C",
-                    "answer": "I start picking a fight.",
-                    "correct": False
-                }, {
-                    "order": "A",
-                    "answer": "I start filming and publicly shame them on social media.",
-                    "correct": False
-                }
-            ]
-        }, {
-			"name": "question7",
-            "question": "How long do I have to wash my hands?",
-            "answers": [{
-                    "order": "C",
-                    "answer": "Until I sang 'Happy Birthday' twice.",
-                    "correct": True
-                }, {
-                    "order": "B",
-                    "answer": "At least 60 seconds.",
-                    "correct": False
-                }, {
-                    "order": "A",
-                    "answer": "Washing your hands for 10 seconds is long enough.",
-                    "correct": False
-                }, {
-                    "order": "D",
-                    "answer": "Until I don't feel like washing my hands any longer.",
-                    "correct": False
-                }
-            ]
+            "order": "B",
+            "answer": "We are taking the kids to the beach for the day.",
+            "correct": False
         }
+        ]
+    }, {
+        "name": "question2",
+        "question": "Which bike trip am I allowed to do?",
+        "answers": [{
+            "order": "A",
+            "answer": "60 kilometers all by myself.",
+            "correct": True
+        }, {
+            "order": "C",
+            "answer": "40 kilometers with a group of cyclists.",
+            "correct": False
+        }, {
+            "order": "B",
+            "answer": "10 kilometers with the whole family on Sunday.",
+            "correct": False
+        }, {
+            "order": "D",
+            "answer": "A bike trip to the parc to enjoy a sunny picnic.",
+            "correct": False
+        }
+        ]
+    }, {
+        "name": "question3",
+        "question": "When do I have the obligation to wash my hands?",
+        "answers": [{
+            "order": "A",
+            "answer": "When I come home from the store.",
+            "correct": True
+        }, {
+            "order": "C",
+            "answer": "Every half hour.",
+            "correct": False
+        }, {
+            "order": "D",
+            "answer": "In the morning when I wake up.",
+            "correct": False
+        }, {
+            "order": "B",
+            "answer": "I just have to make sure to wash them at least 10 times a day",
+            "correct": False
+        }
+        ]
+    }, {
+        "name": "question4",
+        "question": "Which remedies are helpful to protect yourself against COVID-19?",
+        "answers": [{
+            "order": "B",
+            "answer": "Stricltly follow government regulations.",
+            "correct": True
+        }, {
+            "order": "A",
+            "answer": "Absorb an unusually large amount of vitamin C.",
+            "correct": False
+        }, {
+            "order": "C",
+            "answer": "Drink lots of alcohol.",
+            "correct": False
+        }, {
+            "order": "D",
+            "answer": "Smoke sigarettes.",
+            "correct": False
+        }
+        ]
+    }, {
+        "name": "question5",
+        "question": "Children are still allowed to ..",
+        "answers": [{
+            "order": "B",
+            "answer": "Play in the garden with other children of the same household.",
+            "correct": True
+        }, {
+            "order": "A",
+            "answer": "Play in the garden with the neighbours' children.",
+            "correct": False
+        }, {
+            "order": "D",
+            "answer": "Play with other children on the terrains of the local youth movement.",
+            "correct": False
+        }, {
+            "order": "C",
+            "answer": "Play in a public outdoor playground with children of the same household.",
+            "correct": False
+        }
+        ]
+    }, {
+        "name": "question6",
+        "question": "What do you do when you see that others are not following the rules?",
+        "answers": [{
+            "order": "D",
+            "answer": "I instruct them to behave differently and contact local authorities when they don't want to listen.",
+            "correct": True
+        }, {
+            "order": "B",
+            "answer": "I cheerfully join them.",
+            "correct": False
+        }, {
+            "order": "C",
+            "answer": "I start picking a fight.",
+            "correct": False
+        }, {
+            "order": "A",
+            "answer": "I start filming and publicly shame them on social media.",
+            "correct": False
+        }
+        ]
+    }, {
+        "name": "question7",
+        "question": "How long do I have to wash my hands?",
+        "answers": [{
+            "order": "C",
+            "answer": "Until I sang 'Happy Birthday' twice.",
+            "correct": True
+        }, {
+            "order": "B",
+            "answer": "At least 60 seconds.",
+            "correct": False
+        }, {
+            "order": "A",
+            "answer": "Washing your hands for 10 seconds is long enough.",
+            "correct": False
+        }, {
+            "order": "D",
+            "answer": "Until I don't feel like washing my hands any longer.",
+            "correct": False
+        }
+        ]
+    }
     ]
 }
 
